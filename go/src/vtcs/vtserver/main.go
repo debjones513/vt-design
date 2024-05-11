8cb960f3c8f5db6cf39e7a8b13461ab0eecbe23c
Deb Jones <debjones513@gmail.com>
2024-05-11 10:19:12 -0700

    Stubbing web server

--- go/src/vtcs/vtserver/main.go
+++ go/src/vtcs/vtserver/main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 	"vt-design/go/src/vtcs/internal/exe_upload"
 )
 
-func loadExe(name string) *exe_upload.ExeUpload {
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Web server is running %s!", r.URL.Path[1:])
+}
+
+func testLoadExe(name string) *exe_upload.ExeUpload {
 	ep := new(exe_upload.ExeUpload)
 	ep.ExeName = name + ".txt"
 	ep.ExeBytes, _ = os.ReadFile(ep.ExeName)
@@ -14,7 +20,11 @@ func loadExe(name string) *exe_upload.ExeUpload {
 	return ep
 }
 func main() {
-	eps := loadExe("vtserver_test")
+	eps := testLoadExe("vtserver_test")
 	fmt.Println(eps.ExeName)
 	fmt.Println(eps.ExeBytes)
+
+	// Start the web server
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
