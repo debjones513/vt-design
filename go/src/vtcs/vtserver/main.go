def0a25132f40f4069bbeb9e79f3312fb1a7cf03
Deb Jones <debjones513@gmail.com>
2024-05-11 13:44:54 -0700

    Upload handler stub

--- go/src/vtcs/vtserver/main.go
+++ go/src/vtcs/vtserver/main.go
@@ -8,23 +8,40 @@ import (
 	"vt-design/go/src/vtcs/internal/exe_upload"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Web server is running %s!", r.URL.Path[1:])
 }
 
-func testLoadExe(name string) *exe_upload.ExeUpload {
-	ep := new(exe_upload.ExeUpload)
-	ep.ExeName = name + ".txt"
-	ep.ExeBytes, _ = os.ReadFile(ep.ExeName)
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	object_name := r.URL.Path[len("/upload/"):]
+	p, _ := loadExe(object_name)
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.ExeName, p.ExeBytes)
+}
 
-	return ep
+func loadExe(name string) (*exe_upload.ExeUpload, error) {
+
+	data, err := os.ReadFile("/Users/debjo/GitHub/vt-design/go/bin/" + name)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ep := new(exe_upload.ExeUpload)
+	ep.ExeBytes = make([]byte, len(data))
+	n := copy(ep.ExeBytes, data)
+	if n != len(data) {
+		log.Fatal(err)
+	}
+	ep.ExeName = name
+
+	return ep, nil
 }
 func main() {
-	eps := testLoadExe("vtserver_test")
-	fmt.Println(eps.ExeName)
-	fmt.Println(eps.ExeBytes)
+	//eps := loadExe("vtserver_test")
+	//fmt.Println(eps.ExeName)
+	//fmt.Println(eps.ExeBytes)
 
 	// Start the web server
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/", defaultHandler)
+	http.HandleFunc("/upload/", uploadHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil)) // http://localhost:8080/
 }
