39f08fad11ce6acfd8b4bda9ff26faeb8a6f5980
Deb Jones <debjones513@gmail.com>
2024-05-12 14:39:32 -0700

    clean up

--- go/src/vtcs/vtserver/main.go
+++ go/src/vtcs/vtserver/main.go
@@ -10,7 +10,10 @@ import (
 const FmtDefaultHandler string = `
 <div> <p style="color:green;"> &nbsp; &nbsp; <b> Web server is running %s! </b> </p> </div>
 <div> <p style="color:green;"> &nbsp; &nbsp; To test: http://localhost:8080/upload/vtserver_test </p> </div>
+`
 
+const FmtDefaultErrorHandler string = `
+<div> <p style="color:red;"> &nbsp; &nbsp; <b> ERROR! </b> </p> </div>
 `
 
 const FmtUploadHandler string = `
@@ -20,27 +23,34 @@ const FmtUploadHandler string = `
 `
 
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
+
 	fmt.Fprintf(w, FmtDefaultHandler, r.URL.Path[1:])
 }
 
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
 
 	object_name := r.URL.Path[len("/upload/"):]
-	p, _ := loadExe(object_name)
-	//fmt.Fprintf(w, "<p style=\"color:green;\"> &nbsp; &nbsp; <b> Filename </b></p> <p style=\"color:green;\"> &nbsp; &nbsp; %s</p> <div><p style=\"color:blue;\"> &nbsp; &nbsp; <b> Sha256 </b> </p> <p style=\"color:blue;\"> &nbsp; &nbsp; %x</p></div><div><p style=\"color:black;\"> &nbsp; &nbsp; <b> Bytes </b></p> <p style=\"color:gray;\"> &nbsp; &nbsp; %x</p></div>", p.ExeName, p.ExeSha256, p.ExeBytes)
+
+	p, err := loadExeOrUrl(object_name)
+	if err != nil {
+		fmt.Fprintf(w, FmtDefaultErrorHandler)
+		log.Fatal(err)
+		return
+	}
+
 	fmt.Fprintf(w, FmtUploadHandler, p.ExeName, p.ExeSha256, p.ExeBytes)
 }
 
-func loadExe(name string) (*exe_upload.ExeUpload, error) {
+func loadExeOrUrl(object_name string) (*exe_upload.ExeUpload, error) {
 
-	eu, err := exe_upload.Init(name)
-
+	eu, err := exe_upload.Init(object_name)
 	return eu, err
 }
 
 func main() {
 
 	// Start the web server
+
 	http.HandleFunc("/", defaultHandler)
 	http.HandleFunc("/upload/", uploadHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil)) // http://localhost:8080/
