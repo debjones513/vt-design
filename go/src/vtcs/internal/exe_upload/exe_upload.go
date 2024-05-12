4b79a912f5ee7bf9153b20e032524232a82c7744
Deb Jones <debjones513@gmail.com>
2024-05-12 13:45:29 -0700

    Moved exe_upload sub functions to the package

--- go/src/vtcs/internal/exe_upload/exe_upload.go
+++ go/src/vtcs/internal/exe_upload/exe_upload.go
@@ -1,7 +1,93 @@
 package exe_upload
 
+import (
+	"crypto/sha256"
+	"io"
+	"log"
+	"os"
+)
+
 type ExeUpload struct {
 	ExeName   string
 	ExeBytes  []byte
 	ExeSha256 []byte
+}
+
+func (eu *ExeUpload) SetExeName(name string) error {
+
+	// Set the file name
+
+	eu.ExeName = "/Users/debjo/GitHub/vt-design/go/bin/" + name
+
+	return nil
+}
+
+func (eu *ExeUpload) SetExeBytes() error {
+
+	// Set the file bytes
+
+	data, err := os.ReadFile(eu.ExeName)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	eu.ExeBytes = make([]byte, len(data))
+	n := copy(eu.ExeBytes, data)
+	if n != len(data) {
+		log.Fatal(err)
+		return err
+	}
+
+	return nil
+}
+
+func (eu *ExeUpload) SetExeSha256() error {
+
+	// Set the file hash signature
+
+	f, err := os.Open(eu.ExeName)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	defer f.Close()
+
+	h := sha256.New()
+	if _, err := io.Copy(h, f); err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	eu.ExeSha256 = make([]byte, len(h.Sum(nil)))
+	copy(eu.ExeSha256, h.Sum(nil))
+
+	return nil
+}
+
+func Init(name string) (*ExeUpload, error) {
+
+	var err error
+
+	eu := new(ExeUpload)
+
+	err = eu.SetExeName(name)
+	if err != nil {
+		log.Fatal(err)
+		return eu, err
+	}
+
+	err = eu.SetExeBytes()
+	if err != nil {
+		log.Fatal(err)
+		return eu, err
+	}
+
+	err = eu.SetExeSha256()
+	if err != nil {
+		log.Fatal(err)
+		return eu, err
+	}
+
+	return eu, nil
 }
--- go/src/vtcs/vtserver/main.go
+++ go/src/vtcs/vtserver/main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"crypto/sha256"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
-	"os"
 	"vt-design/go/src/vtcs/internal/exe_upload"
 )
 
@@ -18,54 +15,16 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 
 	object_name := r.URL.Path[len("/upload/"):]
 	p, _ := loadExe(object_name)
-	fmt.Fprintf(w, "<p style=\"color:green;\">Filename: %s</p><div><p style=\"color:blue;\">Sha256: %s</p></div><div><p style=\"color:black;\">Bytes:</p> <p style=\"color:gray;\">%s</p></div>", p.ExeName, p.ExeSha256, p.ExeBytes)
+	fmt.Fprintf(w, "<p style=\"color:green;\"> &nbsp; &nbsp; <b> Filename </b></p> <p style=\"color:green;\"> &nbsp; &nbsp; %s</p> <div><p style=\"color:blue;\"> &nbsp; &nbsp; <b> Sha256 </b> </p> <p style=\"color:blue;\"> &nbsp; &nbsp; %x</p></div><div><p style=\"color:black;\"> &nbsp; &nbsp; <b> Bytes </b></p> <p style=\"color:gray;\"> &nbsp; &nbsp; %x</p></div>", p.ExeName, p.ExeSha256, p.ExeBytes)
 }
 
 func loadExe(name string) (*exe_upload.ExeUpload, error) {
 
-	ep := new(exe_upload.ExeUpload)
+	eu, err := exe_upload.Init(name)
 
-	//
-	// Set the file name
-	//
-
-	ep.ExeName = "/Users/debjo/GitHub/vt-design/go/bin/" + name
-
-	//
-	// Set the file bytes
-	//
-
-	data, err := os.ReadFile(ep.ExeName)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	ep.ExeBytes = make([]byte, len(data))
-	n := copy(ep.ExeBytes, data)
-	if n != len(data) {
-		log.Fatal(err)
-	}
-
-	//
-	// Set the file hash signature
-	//
-
-	f, err := os.Open(ep.ExeName)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
-
-	h := sha256.New()
-	if _, err := io.Copy(h, f); err != nil {
-		log.Fatal(err)
-	}
-
-	ep.ExeSha256 = make([]byte, len(h.Sum(nil)))
-	copy(ep.ExeSha256, h.Sum(nil))
-
-	return ep, nil
+	return eu, err
 }
+
 func main() {
 
 	// Start the web server
